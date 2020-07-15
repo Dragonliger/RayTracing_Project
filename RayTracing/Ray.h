e6d33fae1684316206badb148d823eb159612d94
Alex Castelan <alex_castelan@thecdm.ca>
2020-07-14 23:58:04 -0700

    Adding the Ray class, it helps simulate a ray of light moving in tridimensional space.

--- RayTracing/Ray.h
+++ RayTracing/Ray.h
@@ -10,6 +10,15 @@ public:
 		: orig(origin), dir(direction)
 	{}
 
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+
+	// Function P(t) =  A + tB
+	// it helps simulate the movement of the ray on the scene.
+	point3 at(double t) const {
+		return orig + t * dir;
+	}
+
 // Members
 public:
 	point3 orig;
