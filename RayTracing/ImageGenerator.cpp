7d0b07e2af62e8e68749be3b115e842596333265
Alex Castelan <alex_castelan@thecdm.ca>
2020-07-16 19:04:31 -0700

    Added rudimentary normals to the sphere based on the rays coming from the camera.

--- RayTracing/ImageGenerator.cpp
+++ RayTracing/ImageGenerator.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 color ray_color(const ray& r);
 uint8_t double_to_imagespace(double x);
-bool hit_sphere(const point3& center, double radius, const ray& r);
+double hit_sphere(const point3& center, double radius, const ray& r);
 
 int main(int argc, char** args) {
 	const auto aspect_ratio = 16.0 / 9.0;
@@ -75,12 +75,16 @@ int main(int argc, char** args) {
 
 // This function finds the color of a specific ray in the scene according to a gradient.
 color ray_color(const ray& r) {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		return color(1, 0, 0);
+	// Check if we hit the sphere and where.
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
 	// This gets the direction of the vector in a unitary format.
 	vec3 unit_direction = unit_vector(r.direction());
 	// Gets the target position of the linear gradient blend only vertically.
-	auto t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 	// Returns a value in a color gradient from white(1,1,1) to blue(0.5,0.7,1.0).
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
@@ -91,11 +95,16 @@ uint8_t double_to_imagespace(double x) {
 	return (int)round(x * 255);
 }
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	auto a = dot(r.direction(), r.direction());
 	auto b = 2.0 * dot(oc, r.direction());
 	auto c = dot(oc, oc) - radius * radius;
 	auto discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0 * a);
+	}
 }
