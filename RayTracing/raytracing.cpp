0ba2446e50bfcf2e9a1aae346484316131ee3aac
Alex Castelan <alex_castelan@thecdm.ca>
2020-07-19 19:33:30 -0700

    Glass balls and depth of field!!

--- RayTracing/raytracing.cpp
+++ RayTracing/raytracing.cpp
@@ -35,17 +35,23 @@ int main(int argc, char** args) {
 	primitive_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_left = make_shared<dielectric>(1.5);
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_ground));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	// Camera
-	camera cam;
+	point3 lookfrom(3, 3, 2);
+	point3 lookat(0, 0, -1);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).magnitude();
+	auto aperture = 2.0;
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 	// Render
 	int index = 0;
